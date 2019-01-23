98106a375592fd23fd2dcd7ae95ab4988d041506
Tola's Terminal <sohbaker@gmail.com>
2019-01-23 14:41:46 +0000

    test passed: creates an object containing selected data returned by the search

--- __tests__/books_test.js
+++ __tests__/books_test.js
@@ -12,12 +12,18 @@ describe('Books', () => {
   });
 
   it('can make a request to books api', async () => {
-    const response = await books.getData('test')
+    const response = await books.doSearch('test')
     expect(response).toBeDefined();
   });
 
   it('returns an object containing data about books mathching the search item', async () => {
-    const response = await books.getData('hello')
+    const response = await books.doSearch('hello')
     expect(typeof response).toEqual('object');
+  });
+
+  it('creates an object containing selected data returned by the search', async () => {
+    const response = await books.doSearch('hello')
+    const data = books.collectData()
+    expect(typeof data).toEqual('object');
   });
 });
--- src/books.js
+++ src/books.js
@@ -6,8 +6,23 @@ export class Books {
     this.apiRequest = new ApiRequest();
   }
 
-  async getData(search) {
-    const dataReturn = await this.apiRequest.makeCall(search);
+  async doSearch(query) {
+    const dataReturn = await this.apiRequest.makeCall(query);
     return dataReturn;
+  }
+
+  async collectData() {
+    const data = await this.doSearch('hello');
+    let bookData = []
+
+    data.items.forEach(function(hash) {
+      bookData.push({
+        title: hash.volumeInfo.title,
+        author: hash.volumeInfo.authors[0],
+        publisher: hash.volumeInfo.authors.publishedDate,
+        image: hash.volumeInfo.imageLinks.thumbnail,
+      })
+    })
+    return bookData;
   }
 }
