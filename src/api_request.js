086bd43dde6d304039b2f13d739de10191dc1236
Tola's Terminal <sohbaker@gmail.com>
2019-01-27 23:12:40 +0000

    responsive to window changes

--- src/api_request.js
+++ src/api_request.js
@@ -1,5 +1,4 @@
 // jshint esversion: 6
-const request = require('request-promise');
 const fetch = require("node-fetch");
 require('dotenv').config();
 
@@ -8,9 +7,9 @@ export class ApiRequest {
     const url = `https://www.googleapis.com/books/v1/volumes?q=${search}`;
 
     try {
-      const response = await fetch(url + "&key=" + process.env.API_KEY);
+      const response = await fetch(`${url}&key=${process.env.API_KEY}`);
       const data = await response.json();
-      if (!data) return `book not found`;
+      if (!data) return 'book not found';
       return data;
     } catch(error) {
       return `Unexpected error occurred: ${error}`;
