33687a1009c5c80c08f1c231015428e80fce8cd5
Tola's Terminal <sohbaker@gmail.com>
2019-01-27 16:15:36 +0000

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
