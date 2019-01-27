b62c3d73275803d1d9b98f07c050aaddb464d061
Tola's Terminal <sohbaker@gmail.com>
2019-01-27 17:31:17 +0000

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 
-// const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
@@ -15,4 +15,4 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-app.listen(process.env.PORT || 5000);
+app.listen(port);
