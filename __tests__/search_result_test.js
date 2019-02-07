e5ead9fd5ba66fe6f006e9fd9c60ec724bac65ea
Tola's Terminal <sohbaker@gmail.com>
2019-02-07 23:43:16 +0000

    tidied files and removed unused code

--- __tests__/search_result_test.js
+++ __tests__/search_result_test.js
@@ -3,8 +3,6 @@ import { ApiRequest } from '../src/api_request';
 import { SearchResult } from '../src/search_result';
 import * as dummyData from './resources/mock_data';
 
-// const api = jest.mock('../src/api_request');
-// const result = jest.mock('../src/search_result');
 let json = JSON.stringify(dummyData);
 json = JSON.parse(json);
 
@@ -14,21 +12,5 @@ describe('SearchResult', () => {
     dummyFunction.mockReturnValueOnce(json);
     dummyFunction();
     expect(dummyFunction).toHaveReturned();
-    // expect(typeof dummyFunction).toHaveReturned('json');
   });
-
-  // it('should collect 5 pieces of data per book returned by search', () => {
-  //
-  // expect().something();
-  // });
-  //
-  // it('should overwrite undefined value for publisher key with unknown', () => {
-  //
-  //   expect().something();
-  // });
-  //
-  // it('should create a new instance of book object', () => {
-  //
-  //   expect().something();
-  // });
 });
